afbc674fa2e6cea3130fb82933099269f3017b32
Hemit1409 <hemitrana1409@gmail.com>
2022-12-30 09:49:53 +0530

    Update sqlqueries.sql

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -1,7 +1,7 @@
 --run first 1
-CREATE SEQUENCE PATIENT_ID
-    increment 1
-    start 1;
+--CREATE SEQUENCE PATIENT_ID
+--    increment 1
+--    start 1;
    
 --   2
 create table Sex(
@@ -14,15 +14,9 @@ INSERT INTO Sex(sex) VALUES('FEMALE');
 INSERT INTO Sex(sex) VALUES('UNKNOWN');
 select * from Sex;
 
-
-
-
-
-
-
 --3
 create table raceTYPE(
-    race_id serial primary key,
+    race_type_id serial primary key,
    race varchar(20)
     
 );
@@ -30,7 +24,6 @@ INSERT INTO raceTYPE(race) VALUES('AFRICAN');
 INSERT INTO raceTYPE(race) VALUES('ASIAN');
 INSERT INTO raceTYPE(race) VALUES('WHITE');
 select * from raceTYPE;
-
 
 
 --4
@@ -54,45 +47,44 @@ INSERT INTO phonetype(phoneType) VALUES('LANDLINE');
 select * from phonetype;
 
 
-
-
 --6
 create table Patients(
---    patient_id SERIAL primary key,
+    patient_id SERIAL primary key,
 --  	chartnumber AS 'CHART' + CAST(patient_id AS VARCHAR(10)) PERSISTED PRIMARY KEY
-	CHART_NUMBER VARCHAR(40) NOT null unique DEFAULT to_char(nextval('PATIENT_ID'), 'CHART0000FM'), 
+--	CHART_NUMBER VARCHAR(40) NOT null unique DEFAULT to_char(nextval('PATIENT_ID'), '"CHART"0000FM'), 
+	CHART_NUMBER varchar(40) not null unique generated always as ('CHART00'||patient_id::text) stored,
 	firstname varchar(100) not null,
     middlename varchar(100) null,
     lastname varchar(100) not null,
-     sex INT,
-     
+    sex_id INT,
     dob date,
     CONSTRAINT fk_sex  
-    FOREIGN KEY(sex)   
+    FOREIGN KEY(sex_id)   
     REFERENCES Sex(sex_id)
 );
 INSERT INTO Patients(firstname,lastname,middlename,dob) VALUES('HEMIT','RANA','S','2000-01-01');
+INSERT INTO Patients(firstname,lastname,middlename,dob) VALUES('KHUSHI','RANA','S','2005-01-01');
 INSERT INTO Patients(firstname,lastname,middlename,dob) VALUES('KHUSHI','RANA','S','2005-01-01');
 select * from Patients;
 
 --7
 create table RACE(
 race_id serial primary key,
-patient_id VARCHAR(40),    
+CHART_NUMBER VARCHAR(40),    
 race_type_id INT,
 	CONSTRAINT fk_patientid  
-    FOREIGN KEY(patient_id)   
+    FOREIGN KEY(CHART_NUMBER)   
     REFERENCES Patients(CHART_NUMBER),
    CONSTRAINT fk_emp_racetype  
     FOREIGN KEY(race_type_id)   
-    REFERENCES raceTYPE(race_id)
+    REFERENCES raceTYPE(race_type_id)
 );
 
 --8
 create table Addresses(
     address_id SERIAL primary key,  
-    patient_id VARCHAR(40) ,
-    address_type INT,
+    CHART_NUMBER VARCHAR(40) ,
+    AddressesType_id INT,
     -- street VARCHAR(200),
     -- city VARCHAR(200),
     zip VARCHAR(200) unique,
@@ -101,14 +93,14 @@ create table Addresses(
     prim BOOLEAN,
     -- PRIMARY KEY(Department_id),  
     CONSTRAINT fk_patient  
-    FOREIGN KEY(patient_id)   
+    FOREIGN KEY(CHART_NUMBER)   
     REFERENCES Patients(CHART_NUMBER),
     CONSTRAINT fk_addresstype  
-    FOREIGN KEY(address_type)   
+    FOREIGN KEY(AddressesType_id)   
     REFERENCES AddressesType(AddressesType_id)  
 );
-INSERT INTO Addresses(patient_id,zip,country,prim) VALUES('HART0001','390022','India',TRUE);
-INSERT INTO Addresses(patient_id,zip,country,prim) VALUES('HART0002','390021','US',FALSE);
+INSERT INTO Addresses(CHART_NUMBER,zip,country,prim) VALUES('CHART001','390022','India',TRUE);
+INSERT INTO Addresses(CHART_NUMBER,zip,country,prim) VALUES('CHART002','390021','US',FALSE);
 select * from Addresses;
 
 --9
@@ -116,18 +108,15 @@ create table phone(
     ph_id serial primary key, 
     phoneID INT,
     phone varchar(10),
-    ph_type INT,
+    type_id INT,
     prim BOOLEAN,
-    CONSTRAINT fk_phone  
+    CONSTRAINT fk_emp_phone  
     FOREIGN KEY(phoneID)   
     REFERENCES Addresses(address_id),
-    CONSTRAINT fk_phonetype  
-    FOREIGN KEY(ph_type)   
+    CONSTRAINT fk_emp_phonetype  
+    FOREIGN KEY(type_id)   
     REFERENCES phonetype(type_id)
 );
-
-
-
 
 --10
 create table patient_zip(
@@ -139,7 +128,7 @@ zip VARCHAR(200) unique,
     street VARCHAR(200),
     CONSTRAINT fk_petient_zip  
     FOREIGN KEY(zip)   
-    REFERENCES Addresses(zip) , 
+    REFERENCES Addresses(zip)  
 );
 
 
@@ -147,12 +136,12 @@ zip VARCHAR(200) unique,
 
 --11
 create table fax(
-    f_ID serial primary key,
-    faxID INT,
+    fax_id serial primary key,
+    address_id INT,
     fax varchar(10),
     prim BOOLEAN,
     CONSTRAINT fk_emp_fax  
-    FOREIGN KEY(faxID)   
+    FOREIGN KEY(address_id)   
     REFERENCES Addresses(address_id)
 );
 
@@ -167,26 +156,27 @@ create table Contact_preference_type(
 -- 13
 create table Contact_preference(
     preference_contact_id serial primary key,
-    peference_type_id varchar(20),
-    patient_id int,
+    peference_type_id INT,
+    CHART_NUMBER varchar(40),
     address_id int,
     phone_id int,
     fax_id int,
     created_on timestamp default current_timestamp not null,
     modified_on timestamp default current_timestamp not null,
     CONSTRAINT fk_patient  
-    FOREIGN KEY(patient_id)   
+    FOREIGN KEY(CHART_NUMBER)   
     REFERENCES Patients(CHART_NUMBER),
     CONSTRAINT fk_address  
     FOREIGN KEY(address_id)   
     REFERENCES Addresses(address_id),
     CONSTRAINT fk_phone  
     FOREIGN KEY(phone_id)   
-    REFERENCES phone(phone_id),
+    REFERENCES phone(ph_id),
      CONSTRAINT fk_fax  
     FOREIGN KEY(fax_id)   
-    REFERENCES fax(f_ID),
+    REFERENCES fax(fax_id),
      CONSTRAINT fk_contactpref  
     FOREIGN KEY(peference_type_id)   
     REFERENCES Contact_preference_type(preference_type_id)
 );
+
