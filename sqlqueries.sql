79b5646a47d54266e01ca7edcc58c75907364f06
Hemit1409 <hemitrana1409@gmail.com>
2022-12-30 14:49:06 +0530

    Update sqlqueries.sql

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -199,12 +199,16 @@ select * from Contact_preference;
 
 -------(1)
 Create View information as
-Select t1.firstname, t1.lastname, t1.dob, t1.chart_number, t2.sex, t4.race
+Select t1.firstname, t1.lastname, t1.dob, t1.chart_number, t2.sex, t4.race, t5.country, t5.zip, t6.city, t6._state, t6.street, t8.phone
 From
     Patients as t1
     LEFT JOIN sex as t2 ON t1.sex_id = t2.sex_id
     LEFT JOIN RACE as t3 ON t1.CHART_NUMBER = t3.CHART_NUMBER
-    LEFT join raceTYPE as t4 on t1.CHART_NUMBER = t3.CHART_NUMBER and t3.race_type_id=t4.race_type_id;
+    LEFT join raceTYPE as t4 on t1.CHART_NUMBER = t3.CHART_NUMBER and t3.race_type_id=t4.race_type_id
+    LEFT JOIN Contact_preference as t7 on t1.CHART_NUMBER = t7.CHART_NUMBER
+    LEFT join Addresses as t5 on t1.CHART_NUMBER = t5.CHART_NUMBER and t7.address_id = t5.address_id
+    LEFT join patient_zip as t6 on t5.zip = t6.zip
+    LEFT join phone as t8 on t8.ph_id = t7.phone_id;
     -- LEFT JOIN Contact_preference as t4 ON t1.;
     
 SELECT * FROM information;
@@ -217,20 +221,43 @@ select firstname,lastname,dob,sex_id,count(concat(firstname,lastname,dob,sex_id)
 
 
 ------(3)
+CREATE PROCEDURE InsertPatientData(fname varchar(100), mname varchar(100), lname varchar(100),
+                          s_id INT, do_b date)
+LANGUAGE SQL
+AS $$
+    INSERT INTO Patients(firstname,lastname,middlename,sex_id,dob) VALUES(fname,mname,lname,s_id,do_b);   
+$$;
+
+CALL InsertPatientData('SANJAY','RANA','T',1,'1975-01-01');
 
 
-
-
+select * from Patients;
 -------(4)
 
 
 
 
 -----(5)
+-- CREATE PROCEDURE SomeName(@UserStart DATETIME, @UserEnd DATETIME) 
+-- AS BEGIN
 
+-- SELECT somestuff
+-- FROM sometable
+-- WHERE somedate BETWEEN @UserStart AND @UserEnd
 
+-- END
 
-
+-- CREATE PROCEDURE searchByPhone(@number varchar(10)) 
+-- AS BEGIN
+SELECT 
+    t3.phone,firstname,lastname,dob,sex_id
+FROM 
+    Patients as t1
+    LEFT JOIN Addresses as t2 ON t1.CHART_NUMBER = t2.CHART_NUMBER
+    LEFT JOIN phone as t3 ON t2.address_id = t3.phoneID
+WHERE 
+    t3.phone = '123456789';
+-- END
 
 
 
